51187b2b91a3a60524416673c56fb4d60e8f0b19
Dmitry <sliceofkekus@icloud.com>
2024-05-02 10:55:12 +0300

    test: module test for rectangle.py added

---
+++ rectangle_tests.py
@@ -0,0 +1,143 @@
+import unittest
+import rectangle
+
+class TestRectangle(unittest.TestCase):
+
+    def test_area_negative_all_sides(self):
+        '''
+            Тест проверяет как функция rectangle.area вычисляет площадь с отрицательными сторонами
+
+            Параметры:
+            
+            Возвращаемое значение:
+
+        '''
+        with self.assertRaises(ValueError):
+            rectangle.area(-1, -1)
+
+
+    def test_area_negative_one_side(self):
+        '''
+            Тест проверяет как функция rectangle.area вычисляет площадь с одной отрицательной стороной
+
+            Параметры:
+            
+            Возвращаемое значение:
+
+        '''
+        with self.assertRaises(ValueError):
+            rectangle.area(-1, 1)
+
+        with self.assertRaises(ValueError):
+            rectangle.area(1, -1)
+
+
+    def test_area_zero_all_sides(self):
+        '''
+            Тест проверяет как функция rectangle.area вычисляет площадь с нулевыми сторонами
+
+            Параметры:
+            
+            Возвращаемое значение:
+
+        '''
+        res = rectangle.area(0, 0)
+        self.assertEqual(res, 0)
+
+    def test_area_zero_one_side(self):
+        '''
+            Тест проверяет как функция rectangle.area вычисляет площадь с одной нулевой стороной
+
+            Параметры:
+            
+            Возвращаемое значение:
+
+        '''
+        res = rectangle.area(0, 1)
+        self.assertEqual(res, 0)
+
+        res = rectangle.area(1, 0)
+        self.assertEqual(res, 0)
+
+
+    def test_area_positive_sides(self):
+        '''
+            Тест проверяет как функция rectangle.area вычисляет площадь с одной нулевой стороной
+
+            Параметры:
+            
+            Возвращаемое значение:
+
+        '''
+        res = rectangle.area(1, 1)
+        self.assertEqual(res, 1)
+
+
+    def test_perimeter_negative_all_sides(self):
+        '''
+            Тест проверяет как функция rectangle.perimeter вычисляет периметр с отрицательными сторонами
+
+            Параметры:
+            
+            Возвращаемое значение:
+
+        '''
+        with self.assertRaises(ValueError):
+            rectangle.perimeter(-1, -1)
+
+    def test_perimeter_negative_one_side(self):
+        '''
+            Тест проверяет как функция rectangle.perimeter вычисляет периметр с одной отрицательной стороной
+
+            Параметры:
+            
+            Возвращаемое значение:
+
+        '''
+        with self.assertRaises(ValueError):
+            rectangle.perimeter(-1, 1)
+
+        with self.assertRaises(ValueError):
+            rectangle.perimeter(1, -1)
+
+
+    def test_perimeter_zero_all_sides(self):
+        '''
+            Тест проверяет как функция rectangle.perimeter вычисляет периметр с нулевыми сторонами
+
+            Параметры:
+            
+            Возвращаемое значение:
+
+        '''
+        res = rectangle.perimeter(0, 0)
+        self.assertEqual(res, 0)
+
+
+    def test_perimeter_zero_one_side(self):
+        '''
+            Тест проверяет как функция rectangle.perimeter вычисляет периметр с одной нулевой стороной
+
+            Параметры:
+            
+            Возвращаемое значение:
+
+        '''
+        with self.assertRaises(ValueError):
+            rectangle.perimeter(1, 0)
+
+        with self.assertRaises(ValueError):
+            rectangle.perimeter(0, 1)
+
+
+    def test_perimeter_positive_sides(self):
+        '''
+            Тест проверяет как функция rectangle.perimeter вычисляет периметр с положительными сторонами
+
+            Параметры:
+            
+            Возвращаемое значение:
+
+        '''
+        res = rectangle.perimeter(1, 2)
+        self.assertEqual(res, 6)
